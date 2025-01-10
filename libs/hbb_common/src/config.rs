bd1f738a4b05880dad913b344026be575bc80ee6
cqhskj <alex@cqhskj.cn>
2025-01-10 09:02:29 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["erp.cqhskj.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "64bHycJO2c4UvDYwopCFl8WdX97z7LoPhuTUvpOZmc8=";
+pub const PUBLIC_RS_PUB_KEY: &str = "ZaghdFS6zFblnhyYtwZHARFjp5w+OMudvJ3cy9sidfQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
